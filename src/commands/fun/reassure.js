9fc572611f13ec90b644d6d15aa3b4e95f4d1f43
sagarambati <sagarambati734@gmail.com>
2022-12-03 23:06:35 +0530

    add reassure command

---
+++ src/commands/fun/reassure.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Helper = require('./../../classes/helper');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('reassure')
+        .setDescription('Send a random reassuring message to another user or yourself')
+        .addUserOption((option) => 
+            option.setName('who')
+                .setDescription('Who do you want to reassure?')
+                .setRequired(false)
+        ),
+
+    /**
+     * Execute the reassure command
+     * @param interaction
+     * @param client
+     * @returns {Promise<void>}
+    */
+
+    async execute(interaction, client) {
+
+        //Defer the reply
+        await interaction.deferReply();
+
+        var mention='';
+
+        if (interaction.options.getUser('who'))
+            mention = interaction.options.getUser('who');
+        else
+            mention = `<@${interaction.user.id}>`;
+
+        //get the reassure msgs from JSON assets
+        const messages = Helper.getJSONAsset('reassure');
+
+        //get a random number between 0 and {messages} length and pick a random msg
+        const rand = Math.floor(Math.random() * messages.length);
+        const quote = messages[rand];
+
+        //send the reply
+        await interaction.editReply({ content:  `${mention}, ${quote}`});
+    }
+}
