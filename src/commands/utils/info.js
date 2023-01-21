01269bed74aca0061261da36125a61cc102f91e4
Conn Warwicker <conn@cmrwarwicker.com>
2023-01-21 20:12:37 +0000

    WB-43: Fix incorrect "Active Sprints" in info command + fix numbers displayed without commas.

--- src/commands/utils/info.js
+++ src/commands/utils/info.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const EmbeddedMessage = require('../../classes/embed');
 const HumanDate = require('human-date');
 const moment = require('moment');
+const Helper = require('../../classes/helper');
 require('dotenv').config();
 
 module.exports = {
@@ -38,14 +39,14 @@ module.exports = {
         fields.push({ name: 'Server Time', value: moment(now).format('DD-MMM-YYYY, HH:mm'), inline: true });
         fields.push({ name: 'Shard #', value: shardID.toString(), inline: true });
 
-        const active_sprints = await db.get_sql('SELECT COUNT(id) as cnt FROM sprints WHERE completed = 0');
+        const active_sprints = await db.get_sql('SELECT COUNT(id) as cnt FROM sprints WHERE completed = 0 AND end >= ?', [Helper.getUnixTimestamp()]);
         const completed_sprints = await db.get_sql('SELECT COUNT(id) as cnt FROM sprints WHERE completed > 0');
 
         Promise.all(promises).then(results => {
 
             let stats = [];
-            stats.push(`- Total Servers: ${results[0].reduce((acc, guildCount) => acc + guildCount, 0)}`);
-            stats.push(`- Total Users: ${results[1].reduce((acc, memberCount) => acc + memberCount, 0)}`);
+            stats.push(`- Total Servers: ${results[0].reduce((acc, guildCount) => acc + guildCount, 0).toLocaleString()}`);
+            stats.push(`- Total Users: ${results[1].reduce((acc, memberCount) => acc + memberCount, 0).toLocaleString()}`);
             stats.push(`- Total Active Sprints: ${active_sprints.cnt.toLocaleString()}`);
             stats.push(`- Total Completed Sprints: ${completed_sprints.cnt.toLocaleString()}`);
             fields.push({ name: 'General Statistics', value: stats.join("\n") });
